96ad2fe47f48918b43fc7dacfb25199ce4f1a887
MohamedFares09 <mofares010@gmail.com>
2025-10-13 15:46:12 +0300

    main

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,15 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(const WhatsAppClone());
 }
+
 class WhatsAppClone extends StatelessWidget {
   const WhatsAppClone({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('WhatsApp Clone Home Screen'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(body: Center(child: Text('WhatsApp Clone Home Screen'))),
     );
   }
 }
